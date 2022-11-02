33c272392efa328b7cd544709ae2f55b07357aa3
lkb <lkb951103@gmail.com>
2022-11-02 13:54:52 +0900

    router err update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import AboutVue from "@/views/About.vue";
 import ContactVue from "@/views/Contact.vue";
-import IndexVue from "@/views/Index.vue";
 import MyHomeVue from "@/views/MyHome.vue";
 import NotFoundVue from "@/views/NotFound.vue";
 import ServiceVue from "@/views/Service.vue";
@@ -16,50 +15,43 @@ import { ROUTER_NAME } from "./route.name";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: ROUTER_NAME.ROOT,
-    component: IndexVue,
-    children: [
-      {
-        path: "",
-        name: ROUTER_NAME.HOME,
-        component: MyHomeVue,
-      },
-      {
-        path: "/about",
-        name: ROUTER_NAME.ABOUT,
-        component: AboutVue,
-      },
-      {
-        path: "/service",
-        name: ROUTER_NAME.SERVICE,
-        component: ServiceVue,
-      },
-      {
-        path: "/skill",
-        name: ROUTER_NAME.SKILL,
-        component: SkillVue,
-      },
-      {
-        path: "/contact",
-        name: ROUTER_NAME.CONTACT,
-        component: ContactVue,
-      },
-      {
-        path: "/test",
-        name: ROUTER_NAME.TEST,
-        component: TestVue,
-      },
-    ],
+    name: ROUTER_NAME.HOME,
+    component: MyHomeVue,
   },
-  // {
-  //   path: "/:pathMatch(.*)*",
-  //   name: ROUTER_NAME.NOTFOUND,
-  //   component: NotFoundVue,
-  // },
+  {
+    path: "/about",
+    name: ROUTER_NAME.ABOUT,
+    component: AboutVue,
+  },
+  {
+    path: "/service",
+    name: ROUTER_NAME.SERVICE,
+    component: ServiceVue,
+  },
+  {
+    path: "/skill",
+    name: ROUTER_NAME.SKILL,
+    component: SkillVue,
+  },
+  {
+    path: "/contact",
+    name: ROUTER_NAME.CONTACT,
+    component: ContactVue,
+  },
+  {
+    path: "/test",
+    name: ROUTER_NAME.TEST,
+    component: TestVue,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: ROUTER_NAME.NOTFOUND,
+    component: NotFoundVue,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory("/myplace/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
--- src/router/route.name.ts
+++ src/router/route.name.ts
@@ -1,5 +1,4 @@
 export const ROUTER_NAME = {
-  ROOT: "index",
   HOME: "home",
   ABOUT: "about",
   SERVICE: "service",
